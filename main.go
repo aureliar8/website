bf68842064de38974181da4cde7f151431a819a5
Aurelien DEROIDE <aurelien.deroide@free.fr>
2021-07-15 22:21:37 +0200

    Start server with environment variable as port

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Welcome to aureliar's website")
 	})
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Println("PORT environement variable not found ")
+	}
+	log.Println(http.ListenAndServe(":"+port, nil))
 }
