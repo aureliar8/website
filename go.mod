c6e1a5f338550a145be326cb6621443aa7ad8006
Aurelien DEROIDE <aurelien.deroide@free.fr>
2021-07-20 20:32:26 +0200

    USe errgroup to handle two server deployment

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/aureliar8/website
 
 go 1.14
+
+require golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+
+require golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+
+require golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -12,13 +17,51 @@ func main() {
 	muxhttp.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		http.Redirect(w, req, "https://"+req.Host+req.RequestURI, http.StatusMovedPermanently)
 	})
-	httpsmux := http.NewServeMux()
-	httpsmux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+	muxhttps := http.NewServeMux()
+	muxhttps.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Welcome to aureliar's website")
 
 	})
+	server := http.Server{
+		Addr:    ":4443",
+		Handler: muxhttps,
+		// https://blog.cloudflare.com/exposing-go-on-the-internet/
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		TLSConfig: &tls.Config{
+			NextProtos:       []string{"h2", "http/1.1"},
+			MinVersion:       tls.VersionTLS12,
+			CurvePreferences: []tls.CurveID{tls.CurveP256, tls.X25519},
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			},
+			PreferServerCipherSuites: true,
+		},
+	}
+	g, ctx := errgroup.WithContext(context.Background())
+	g.Go(func() error {
+		if err := server.ListenAndServeTLS("fullchain.pem", "privkey.pem"); err != http.ErrServerClosed {
+			return err
+		}
+		return nil
+	})
+	g.Go(func() error {
+		if err := http.ListenAndServe(":8080", muxhttp); err != http.ErrServerClosed {
+			return err
+		}
+		return nil
+	})
 	go func() {
-		log.Println(http.ListenAndServe(":8080", muxhttp))
+		<-ctx.Done()
+		server.Close()
+		//HttpServer.Close()
 	}()
-	log.Println(http.ListenAndServeTLS(":4443", "fullchain.pem", "privkey.pem", httpsmux))
+	log.Println(g.Wait())
 }
